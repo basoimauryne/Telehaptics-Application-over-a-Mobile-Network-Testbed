cbe70f901fd0f58efe659bcfd4689cb8fda575c9
Maurine Chepkoech <32432592+basoimauryne@users.noreply.github.com>
2023-01-18 23:49:53 +0200

    Update Robotic-Arm-Control.py

--- Robotic-Arm-Control.py
+++ Robotic-Arm-Control.py
@@ -14,6 +14,7 @@ pwm = Adafruit_PCA9685.PCA9685()	# Instantiate the object used to control the pu
 pwm.set_pwm_freq(50)	# Set the frequency of the PWM signal
 program_path = "path to the executable socket initiating code"
 dict={}
+# Read values sent from robotic arm 
 p = Popen([program_path], stdout=PIPE, stdin=PIPE, universal_newlines=True)
 #This is initializing the minimum position values for the 4DOF robotic arm motors in every axis
 xaxis=100
@@ -22,11 +23,14 @@ yaxis2=100
 zaxis=100
 packet = 0
 
+
+# Define TCP socket parameters
 ip = " IP"  # The server's hostname or IP address
 port = "PORT"  # The port used by the server
 connectserver = (ip, port)
 
 
+# function to scale values
 def translate(value, leftmin, leftmax, rightmin, rightmax):
     leftspan = leftmax - leftmin
     rightspan = rightmax -rightmin
@@ -39,8 +43,9 @@ while True:
     s = socket(AF_INET, SOCK_STREAM)
 
     result= p.stdout.readline().strip()
-
+    # Process output from robotic arm
     output=result.split(':')
+    #store arm movement values in dictionary for easy organization and access
     dict[output[0]]=float(output[1])
     current_time= round(time.time()*1000)
     print (current_time,"dict1:",dict)     # DEBUG
@@ -56,27 +61,13 @@ while True:
         zaxis=int(translate(dict['Z'], -0.219729, 0.209877, 100, 560))
     if 'Packet_Label' in dict:
         packet=int(dict['Packet_Label'])
-      
-    if 0 == 1:    # DEBUG
     
-        for i in range(100,xaxis,3):
-           print ("i:",i)
-           pwm.set_pwm(3, 0, i)
-           time.sleep(0.03)
-
-    if 0 == 1:     # DEBUG
-        print ("xaxis:",xaxis)
-        print ("previous_x:",xaxis_prev)
-        for i in range(560,xaxis,-3):
-           print ("i:",i)
-           pwm.set_pwm(3, 0, i)
-           time.sleep(0.03)
 
 
     pwm.set_pwm(0, 0, xaxis)
     pwm.set_pwm(1, 0, yaxis1)
     pwm.set_pwm(2, 0, yaxis2)
-    #Enable connection between the master and slave  in the feedback loop
+    #Enable connection between the master and slave  in the feedback loop and send data
     s.connect(connectserver)
     send_data = packet;
     s.send(str(send_data).encode('utf8'))
